6e9ff9b28ad4d9ba194b5a93d132d740682d10c4
gaoxuxian <3072462354@qq.com>
2018-12-19 15:43:12 +0800

    update

--- commentuilib/src/main/java/lib/ui/SimpleItemView.java
+++ commentuilib/src/main/java/lib/ui/SimpleItemView.java
@@ -5,12 +5,13 @@ import androidx.annotation.NonNull;
 import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 
 public class SimpleItemView extends FrameLayout
 {
-    private TextView mTx;
+    private Button mTx;
 
     public SimpleItemView(@NonNull Context context)
     {
@@ -20,7 +21,7 @@ public class SimpleItemView extends FrameLayout
 
     private void initUI(Context context)
     {
-        mTx = new TextView(context);
+        mTx = new Button(context);
         mTx.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 12);
         mTx.setGravity(Gravity.CENTER);
         FrameLayout.LayoutParams params = new LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
